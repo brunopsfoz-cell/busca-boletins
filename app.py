b25651da21b69224671cd30337baecdae5d0a268
Bruno Pereira <brunopsfoz@gmail.com>
2026-07-24 20:45:33 -0300

    Baixar banco automaticamente

--- app.py
+++ app.py
@@ -2,32 +2,90 @@ from flask import Flask, render_template, request
 import sqlite3
 import os
 import re
+import requests
+
 
 app = Flask(__name__)
 
+
 BANCO = "banco/boletins.db"
+
+URL_BANCO = "https://drive.google.com/uc?export=download&id=1DIdZcUa3DyXuA7kAPG83w0yNxnMNCOoo"
+
+
+def baixar_banco():
+
+    os.makedirs("banco", exist_ok=True)
+
+    print("Baixando banco...")
+
+    resposta = requests.get(URL_BANCO)
+
+    resposta.raise_for_status()
+
+    with open(BANCO, "wb") as arquivo:
+        arquivo.write(resposta.content)
+
+    print("Banco baixado!")
+
+
+def verificar_banco():
+
+    try:
+        conn = sqlite3.connect(BANCO)
+
+        conn.execute(
+            "SELECT count(*) FROM paginas"
+        )
+
+        conn.close()
+
+        print("Banco OK")
+
+    except Exception:
+
+        print("Banco inválido. Baixando novamente...")
+
+        if os.path.exists(BANCO):
+            os.remove(BANCO)
+
+        baixar_banco()
+
+
+
+verificar_banco()
+
 
 
 def conectar():
+
     conn = sqlite3.connect(BANCO)
+
     conn.row_factory = sqlite3.Row
+
     return conn
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     resultados = []
+
     termo = ""
+
 
     if request.method == "POST":
 
         termo = request.form["busca"].strip()
 
+
         if termo:
 
             conn = conectar()
+
             cursor = conn.cursor()
+
 
             consulta = """
             SELECT 
@@ -39,7 +97,9 @@ def index():
             LIMIT 100
             """
 
+
             try:
+
                 cursor.execute(
                     consulta,
                     (termo,)
@@ -47,10 +107,14 @@ def index():
 
                 resultados = cursor.fetchall()
 
+
             except Exception as erro:
+
                 print(erro)
 
+
             conn.close()
+
 
 
     return render_template(
@@ -58,6 +122,7 @@ def index():
         resultados=resultados,
         termo=termo
     )
+
 
 
 if __name__ == "__main__":
